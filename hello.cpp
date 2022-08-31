c45cd39f73014467ba73c122615cbe3994a8d284
Yudhapratama355 <111962863+Yudhapratama355@users.noreply.github.com>
2022-08-31 19:38:09 +0700

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    cout << "nama:yudha"<< endl;
+    cout << "kelas:10PPLG1"<< endl;
+    cout << "============="<< endl;
+    cout << "hasil;"<< 10+5<<endl;
+    cout << " angka1=10"<< endl;
+    cout << " angka2=5"<< endl;
+    cout << " hasil;"<< 10-5<< endl;
+    cout << " hasil;"<< 3*6<< endl;
+    return 0;
+} 
